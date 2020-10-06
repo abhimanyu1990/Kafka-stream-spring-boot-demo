bedf85bef36e13b45dbf0df00d8004891e9b8a8e
abhimanyu1990 <abhimanyu1990@hotmail.com>
2020-10-06 23:40:58 +0530

    initial commit

---
+++ src/main/java/poc/kafka/SpringbootKafkaStream.java
@@ -0,0 +1,13 @@
+package poc.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaStream {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaStream.class, args);
+	}
+
+}
